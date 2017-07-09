77dbeba35cb03330343ee857ff1f5cad4f88ff7d
YZG112358 <yg7@illinois.edu>
2017-07-09 17:24:20 -0500

    second commit

--- index.js
+++ index.js
@@ -18,20 +18,19 @@ app.use(logger('dev'))
 app.use(bodyParser.json()) //parses json, multi-part (file), url-encoded 
  
 app.get('/', function(req, res) {
-  afterLoad('localhost:42000', function(html){
-		fs.writeFile('demofile1.html', html, function(err){
-  			if (err) throw err;
-  			console.log('Saved!');
-  		});
-	});
+  var html=afterLoad('localhost:42000');
+    fs.writeFile('demofile1.html', html, function(err){
+        console.log('Saved!');
+      });
 
-  	
 
-  	fs.readFile('demofile1.html', function(err, data) {
-    	res.writeHead(200, {'Content-Type': 'text/html'});
-    	res.write(data);
-    	return res.end();
-  	});
+    
+
+    fs.readFile('demofile1.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
 })
  
 var server = http.createServer(app)
@@ -42,3 +41,90 @@ reload(server, app)
 server.listen(app.get('port'), function(){
   console.log("Web server listening on port " + app.get('port'));
 });
+
+/*
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var reload = require('reload')
+var afterLoad = require('after-load');
+
+var app = express();
+app.get('/', function (req, res) {
+   afterLoad('mangofrenzylab.com', function(html){
+    fs.writeFile('demofile1.html', html, function(err){
+        if (err) throw err;
+        console.log('Saved!');
+      });
+  });
+
+    fs.readFile('demofile1.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+})
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+   reload(server, app)
+
+})
+*/
+/*
+var options = {
+  host: 'www.google.com',
+  port: 80,
+  path: '/index.html'
+};
+
+http.get(options, function(res) {
+  console.log("Got response: " + res.statusCode);
+  console.log()
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+
+
+
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+http.createServer(function (req, res) {
+  
+  afterLoad('mangofrenzylab.com', function(html){
+    fs.writeFile('demofile1.html', html, function(err){
+        if (err) throw err;
+        console.log('Saved!');
+      });
+  });
+
+    fs.readFile('demofile1.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+}).listen(8080);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
